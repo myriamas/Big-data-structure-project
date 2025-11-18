f2c8b8f0cd83825c4ec217432f3658e05ef04425
Myriam Ait Said <my.aitsaid@gmail.com>
2025-11-18 22:12:24 +0100

    Delete bigdata directory

--- bigdata/models.py
+++
@@ -1,6 +0,0 @@
-class Collection:
-    def __init__(self, name, schema, stats, document_count):
-        self.name = name
-        self.schema = schema
-        self.stats = stats
-        self.document_count = document_count
